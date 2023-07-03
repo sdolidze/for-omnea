1fed55dd17da949ec2aa8b5184a09550a8ae2fa2
Sandro Dolidze <me@sdolidze.ge>
2023-07-03 22:50:48 +0400

    feat: add apollo and mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button } from "@mui/material";
+import { gql, useQuery } from "@apollo/client";
+
+const NOTE_LIST = gql`
+  query NoteList {
+    notes(limit: 5) {
+      id
+      text
+      updated_at
+    }
+  }
+`;
+
+interface Note {
+  id: number;
+  text: string;
+  updated_at: string;
+}
 
 function App() {
+  const { loading, error, data } = useQuery<{ notes: Note[] }>(NOTE_LIST);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  if (data === undefined) {
+    return <p>Missing data</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Button variant="contained">Hello</Button>
+      {data.notes.map(note => (
+        <div key={note.id}>
+          <p>{note.id}</p>
+          <p>{note.text}</p>
+          <p>{note.updated_at}</p>
+        </div>
+      ))}
+    </Box>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import { CssBaseline } from "@mui/material";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const client = new ApolloClient({
+  uri: "https://hiring-challenge.hasura.app/v1/graphql/",
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CssBaseline />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
